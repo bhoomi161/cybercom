0b8c459b0e284b2cc984fcd68e2904cfce0904c4
bhoomi161 <bhoomimehta161@gmail.com>
2021-01-19 18:40:59 +0530

    19-jan-2021

---
+++ Jan-19-html/HTML Practice8/information.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP File</title>
+</head>
+<body>
+	<?php
+		$fnm=$_POST["fnm"];
+		$lnm=$_POST["lnm"];
+		$day=$_POST["day_list"];
+		$month=$_POST["month_list"];
+		$year=$_POST["year_list"];
+		$gender=$_POST["gender"];
+		$email=$_POST["email"];
+		$remail=$_POST["Remail"];
+		$pwd=$_POST["pwd"];
+		$rpwd=$_POST["rpwd"];
+
+		echo "welcome";
+		echo $day;
+
+	?>
+</body>
+</html>
